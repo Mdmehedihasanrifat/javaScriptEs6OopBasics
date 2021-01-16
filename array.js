d5642f6db5b29244d09be25e8af15a4086ace001
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2021-01-16 17:44:24 +0600

    "object"

--- array.js
+++ array.js
@@ -1,1 +1,10 @@
-var array=[]
+var arraysInArrays = [
+    [1, 2, 3],
+    ["Julia", "James"],
+    [true, false, true, false]
+];
+
+
+var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
+donuts.splice(1, 1, "chocolate cruller", "creme de leche");
+console.log(7);
