c46d889fe2f04370530229a567219825970ad95b
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2021-01-21 20:52:16 +0600

    factorial using while and for loop

---
+++ factorial.js
@@ -0,0 +1,20 @@
+let factorial = 4;
+let fact=1;
+let fact1=1;
+
+for(let i=1;i<=factorial;i++){
+
+
+    fact=fact*i;
+}
+console.log(fact);
+
+//while loop
+let i=1;
+while (i<=factorial){
+
+
+    fact1=fact1*i;
+    i++;
+}
+console.log(fact1);
